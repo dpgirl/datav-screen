00e12a3d8c560d2a4e49a4d5bc5b408b5dc412ae
xuewucandy <1571652302@qq.com>
2024-08-25 22:54:39 +0800

    feat: 容器组件调用

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,27 @@
 <script setup></script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <div class="svg-container">
-        <!-- <svg width="50" height="50" style="color: red">
-            <use href="#more"></use>
-          </svg>-->
-        <!-- 这里的svg是自定义的svg组件 -->
-        <CustomSvg></CustomSvg>
-        <Icon prefix="" name="more" :style="{ width: '30px', height: '30px', color: 'red' }" />
-        <Icon prefix="" name="arrow" class="arrow" />
-        <!-- 引入iconfont的图标 -->
-        <Icon name="aixin" class="aixin" />
-      </div>
-      <!-- <Loading width="100" height="100" out-side-color="cornflowerblue" in-side-color="red">
-        <div style="color: cornflowerblue">加载中...</div>
-      </Loading> -->
-      <Loading width="100" height="100">
-        <div style="color: cornflowerblue">加载中...</div>
-      </Loading>
-      <div style="width: 50%; background: #333; color: #fff; text-align: center">
-        <FlyBox duration="5" star-len="100" star-color="rgb(100, 237, 132)">
-          <div>111</div>
-          <div>111</div>
-          <div>111</div>
-          <div>111</div>
-          <div>111</div>
-        </FlyBox>
-      </div>
-      <div style="width: 100%; background: #333; color: #fff; text-align: center">
-        <FlyBox>
-          <div>111</div>
-          <div>111</div>
-          <div>111</div>
-          <div>111</div>
-          <div>111</div>
-        </FlyBox>
-      </div>
-    </div>
-  </header>
+  <datavContainer :options="{ width: 3840, height: 2160 }">
+    <div class="test">6666</div>
+  </datavContainer>
 
   <!-- <RouterView /> -->
 </template>
 
+<style>
+/** 初始化为全屏，尺寸会随着浏览器窗口变化而变化；且需为全局 */
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+  margin: 0 !important;
+  padding: 0 !important;
+}
+.test {
+  font-size: 140px;
+}
+</style>
 <style scoped>
 .svg-container {
   color: cornflowerblue;
